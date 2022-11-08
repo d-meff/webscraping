26d4f9841e61b0c6fc5be0ea028f7b6bab2925c3
Danny <dannythekid1292@gmail.com>
2022-11-08 17:37:17 -0600

    project is done - may need changes later

--- webscraping-movies.py
+++ webscraping-movies.py
@@ -12,9 +12,7 @@ from openpyxl.styles import Font
 webpage = 'https://www.boxofficemojo.com/year/2022/'
 
 page = urlopen(webpage)			
-
 soup = BeautifulSoup(page, 'html.parser')
-
 title = soup.title
 
 print(title.text)
